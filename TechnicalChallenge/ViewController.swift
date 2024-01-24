4a5f8fd8522e6929c4f808680fe35da76848b99b
Aaron Cordero <aacorderoc@hotmail.com>
2024-01-24 14:30:38 -0500

    create project

---
+++ TechnicalChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnicalChallenge
+//
+//  Created by Aaron Cordero on 24/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
